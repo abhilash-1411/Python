f2f592ff5c147ef03f31dd3b49d277258e1ce66b
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-08-08 19:26:34 +0530

    more on decorators

--- decorators.py
+++ decorators.py
@@ -1,17 +1,36 @@
-def div(a,b):
-    return a/b
+import time
+
+# def div(a,b):
+#     return a/b
 
 
 # decorater
-def smart(func):
-    def inner(a,b):
-        if a<b:
-            a,b=b,a
-        return func(a,b)
+# def smart(func):
+#     def inner(a,b):
+#         if a<b:
+#             a,b=b,a
+#         return func(a,b)
     
-    return inner
+#     return inner
 
-div=smart(div)
+# div=smart(div)
 
-result=div(2,4)
-print(result)
+# result=div(2,4)
+# print(result)
+
+def timer(function):
+     def wrapper(*args, **kwargs):
+          start=time.time()
+          result=function(*args, **kwargs)
+          end=time.time()
+          print(f"{function.__name__} ran in {end-start} seconds")
+          return result
+     return wrapper
+
+
+
+@timer
+def example_function(n):
+     time.sleep(n)
+
+example_function(2)
