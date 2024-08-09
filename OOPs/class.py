ec28b8e01b3bdc389e2c0cc468749a953778ffd8
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-08-09 12:30:32 +0530

    class and init method

--- OOPs/class.py
+++ OOPs/class.py
@@ -1,15 +1,21 @@
 class Computer:
+    #  for every object it is called automatically
+     def __init__(self,cpu,ram):
+         self.cpu = cpu
+         self.ram = ram
+        
      def config(self):
-        print("i5,16gb,1TB")
+        print("config is ",self.cpu,self.ram)
 
 
-com1=Computer()
-com2=Computer()
+com1=Computer('i5',16)
+com2=Computer('Ryzen',8)
 
 
-# com1.config()
-Computer.config(com1)
-Computer.config(com2)
+com1.config()
+com2.config()
+# Computer.config(com1)
+# Computer.config(com2)
 
 
 
